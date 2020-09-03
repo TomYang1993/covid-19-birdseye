534242a04bd9a5c45b9641e37acb81bcd3513227
Tom Yang <xy2286@columbia.edu>
2020-09-02 23:50:25 -0400

    add word cloud sample

--- src/components/WordCloud.js
+++ src/components/WordCloud.js
@@ -3,48 +3,53 @@ import React, { useRef, useEffect } from 'react';
 import * as d3 from "d3";
 import cloud from 'd3-cloud';
 
-const words = [
-    {
-      text: 'told',
-      value: 64,
-    },
-    {
-      text: 'mistake',
-      value: 11,
-    },
-    {
-      text: 'thought',
-      value: 16,
-    },
-    {
-      text: 'bad',
-      value: 17,
-    },
-  ]
-
 
 function WordCloud({width, height}) {
 
 
     const ref = useRef();
 
+ 
+
     useEffect(() => {
         const svg = d3.select(ref.current)
-            .attr("width", width)
-            .attr("height", height)
-            .style("border", "1px solid black")
+        .attr("width", width)
+        .attr("height", height)
+        .style("border", "2px solid black")
+
+        const draw = (words) => {
+            svg
+            .append("g")
+              .attr("transform", "translate(" + 250 + "," + 250 + ")")
+            .selectAll("text")
+              .data(words)
+            .enter().append("text")
+              .style("font-size", function(d) { return d.size + "px"; })
+              .style("font-family", "Impact")
+              .attr("text-anchor", "middle")
+              .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+              })
+              .text(function(d) { return d.text; });
+        }
+
+        let layout = cloud()
+        .size([500, 500])
+        .words([
+          "Hello", "world", "normally", "you", "want", "more", "words",
+          "than", "this"].map(function(d) {
+          return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+        }))
+        .padding(5)
+        .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .font("Impact")
+        .fontSize(function(d) { return d.size; })
+        .on("end", draw);
+
+    
+        layout.start();
+      
     }, []);
-
-    // useEffect(() => {
-    //     draw();
-    // }, [data]);
-
-    const draw = () => {
-        
-        const svg = d3.select(ref.current);
-   
-    }
-
 
     return (
         <div className="word-cloud">
