ff4828ff136329dd6441fcf544656cfa6bdc458e
Tom Yang <xy2286@columbia.edu>
2020-10-09 21:02:35 -0400

    Updates

---
+++ precache-manifest.d3f80862ca30d52c32109ffa7e4e93cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05f7c29d65801d2babbb78833043021",
+    "url": "/covid-19-birdseye/index.html"
+  },
+  {
+    "revision": "7903127749f20818f5aa",
+    "url": "/covid-19-birdseye/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9e7fdf16cbc472b97394",
+    "url": "/covid-19-birdseye/static/js/2.9bd2652b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-birdseye/static/js/2.9bd2652b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7903127749f20818f5aa",
+    "url": "/covid-19-birdseye/static/js/main.76c38eef.chunk.js"
+  },
+  {
+    "revision": "5539d88bc5a3a2133292",
+    "url": "/covid-19-birdseye/static/js/runtime-main.8941769c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-birdseye/static/media/logo.5d5d9eef.svg"
+  }
+]);
